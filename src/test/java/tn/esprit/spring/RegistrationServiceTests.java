02ef9276b994ddd85f3fa66a5df64c60a3ce4730
Ahmed BEN HADID <ahmedbhd97@gmail.com>
2023-11-07 13:51:45 +0100

    implement test unitaire and integrate spring app with promethues

--- src/test/java/tn/esprit/spring/RegistrationServiceTests.java
+++ src/test/java/tn/esprit/spring/RegistrationServiceTests.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
 class RegistrationServiceTests {
     @InjectMocks
     private RegistrationServicesImpl registrationService;
